da361d767dfb8392f159fae5044315b1bf4b3ba4
manashbarai <manashjbarai@gmail.com>
2024-12-03 00:57:43 +0530

    Update middleware.js with profile update path

--- middleware.js
+++ middleware.js
@@ -4,6 +4,7 @@ export function middleware(request) {
   const token = request.cookies.get('token'); // Retrieve token from cookies
   const pathname = request.nextUrl.pathname; // Get the current path
 
+
   // Allow requests to the /login route or static files to proceed without checking the token
   if (pathname === '/login'||pathname.startsWith('/reset-password/') || pathname.startsWith('/_next')) {
     return NextResponse.next();
